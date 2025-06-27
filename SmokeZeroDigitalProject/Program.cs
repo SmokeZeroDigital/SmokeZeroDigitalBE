30c444e35666ceff8547fb3267405a8989273595
QuocAn108 <anhoang800140@gmail.com>
2025-06-27 22:08:20 +0700

    done config run swagger

--- SmokeZeroDigitalProject/Program.cs
+++ SmokeZeroDigitalProject/Program.cs
@@ -1,8 +1,6 @@
 using eCommerce_BE.Infrastructure;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
-using SmokeZeroDigitalSolution.Domain.Entites;
 using SmokeZeroDigitalSolution.Infrastructure.DataAccessManager.EFCore.Data;
 using System.Reflection;
 
@@ -58,6 +56,11 @@ namespace SmokeZeroDigitalProject
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
+            }
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
             }
 
             app.UseHttpsRedirection();
