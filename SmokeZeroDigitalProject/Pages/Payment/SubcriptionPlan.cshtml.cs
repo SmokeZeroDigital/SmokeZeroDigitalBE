aa62dfc368e6dbc63da7b42fadaaa1924500313d
QuocAn108 <anhoang800140@gmail.com>
2025-07-09 22:06:14 +0700

    feat: Finish Plan Page

---
+++ SmokeZeroDigitalProject/Pages/Payment/SubcriptionPlan.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SmokeZeroDigitalProject.Pages.Page
+{
+    public class SubcriptionPlanModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
