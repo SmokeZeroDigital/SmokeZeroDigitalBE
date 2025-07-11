1b99c0d4c60c38a17dcdf9b7554d9c2ee47677c6
TriNgCao <tringuyencao.se@gmail.com>
2025-07-12 00:39:53 +0700

    implement noti feature

---
+++ SmokeZeroDigitalSolution.Infrastructure/ExternalServices/Notification/DI.cs
@@ -0,0 +1,14 @@
+﻿using SmokeZeroDigitalSolution.Application.Features.NotificationManager.Interface;
+
+namespace SmokeZeroDigitalSolution.Infrastructure.ExternalServices.Notification
+{
+    public static class DI
+    {
+        public static IServiceCollection RegisterNotiFeature(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddScoped<INotiRepo, NotiRepository>();
+            services.AddScoped<INotiService, NotiService>();
+            return services;
+        }
+    }
+}
